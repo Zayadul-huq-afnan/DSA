f5e0a6fd6636ce2e1be003f57f416ba6ace285f8
Zayadul-huq-afnan <76451807+Zayadul-huq-afnan@users.noreply.github.com>
2025-04-10 03:24:10 +0600

    Update bfs.cpp

--- Graphs/algorithms/bfs.cpp
+++ Graphs/algorithms/bfs.cpp
@@ -1,1 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5;
+
+vector<int> adj[N];
+
+bool visited[N];
+
+void bfs(int node){
+
+    queue<int>q;
+    visited[node] = true;
+    q.push(node);
+
+    while(!q.empty()){
+
+        int curr_node = q.front();
+        q.pop();
+
+        for(int v : adj[curr_node]){
+
+            if(visited[v])continue;
+
+            visited[v] = true;
+            q.push(v);
+        }
+    }
+
+}
 
