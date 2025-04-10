fe86a7c219c282c3158b52344bbd5a10867a2a7a
Zayadul-huq-afnan <76451807+Zayadul-huq-afnan@users.noreply.github.com>
2025-04-11 00:31:15 +0600

    Update dfs.cpp

--- Graphs/algorithms/dfs.cpp
+++ Graphs/algorithms/dfs.cpp
@@ -1,1 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5;
+
+vector<int> adj[N];
+
+bool visited[N];
+
+
+void dfs(int node){
+
+    visited[node] = true;
+
+    for(int x : adj[node]){
+
+        if(visited[x]) continue;
+
+        dfs(x);
+    }
+
+}
 
