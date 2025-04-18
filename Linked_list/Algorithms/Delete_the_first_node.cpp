9ac5808a62eb381931da31f75c550ebe7ba05782
Zayadul-huq-afnan <76451807+Zayadul-huq-afnan@users.noreply.github.com>
2025-04-19 02:59:21 +0600

    Create Delete_the_first_node.cpp

---
+++ Linked_list/Algorithms/Delete_the_first_node.cpp
@@ -0,0 +1,75 @@
+//here we will delete the head
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+class node{
+
+public:
+    //make the data and link to next node
+    //public so that you can access it from outside
+
+    int data;
+
+    node *next; //this is a pointer.
+
+    //this is the constructor
+    node(int num){
+        data = num;
+        next = NULL;
+    }
+
+};
+
+
+int main(){
+
+    int arr[] = {1, 2 , 3 , 4 , 5};
+
+    //take a head
+    node *head = NULL;
+    node *tail;
+
+    for(int i = 0 ; i < 5 ; i++){
+
+        //head NULL means there is currently no linked list
+        if(head == NULL){
+            //creating the first node of the linked list
+            head = new node(arr[i]);
+
+            //tail always locate at the last node of the list
+            tail = head;
+        }
+
+        else{
+
+            //create e new node and save it address in the tmp pointer
+            node *tmp = new node(arr[i]);
+
+            //save the address of the new node in the current node
+            tail->next = tmp;
+
+            //now move the tail to the newly added node which is
+            //the current last node of the list
+
+            tail = tail->next;
+
+        }
+    }
+
+    //delete the head;
+    node *tmp = head;
+    head = head->next;
+    delete tmp;
+
+    node *curr = head;
+
+    while(curr != NULL){
+        cout<<curr->data<<"\n";
+
+        curr = curr->next;
+    }
+}
